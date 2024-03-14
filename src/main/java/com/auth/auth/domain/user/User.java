104a6dea60dc58971e265b9d61bf27d0ec436623
Wendel dos Santos Nunes <wendelnunes9999@gmail.com>
2024-03-13 21:29:04 -0300

    feat: Creation of the UserController and first get method, but without bank implementation

--- src/main/java/com/auth/auth/domain/user/User.java
+++ src/main/java/com/auth/auth/domain/user/User.java
@@ -1,17 +1,18 @@
 package com.auth.auth.domain.user;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 
-@Table(name="user")
-@Entity(name="user")
+@Table(name="user_auth")
+@Entity(name="user_auth")
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 @EqualsAndHashCode(of = "id")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
