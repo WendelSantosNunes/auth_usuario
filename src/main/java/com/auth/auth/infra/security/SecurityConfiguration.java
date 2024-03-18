2e423a9eb9f4b2ea93bffbab94fc0b7d754c3cad
Wendel dos Santos Nunes <wendelnunes9999@gmail.com>
2024-03-18 17:11:52 -0300

    feat: Adding route documentation with swagger

--- src/main/java/com/auth/auth/infra/security/SecurityConfiguration.java
+++ src/main/java/com/auth/auth/infra/security/SecurityConfiguration.java
@@ -28,6 +28,7 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.GET,"/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
